5ce3bd2e5686c88650142021b1a10437041d0d69
kermage <genealyson.torcende@gmail.com>
2023-04-27 11:57:09 +0800

    the per attribute and quantity feature

--- src/Actions.php
+++ src/Actions.php
@@ -7,11 +7,94 @@
 
 namespace CardanoPress\RolesManager;
 
+use CardanoPress\Helpers\WalletHelper;
 use CardanoPress\Interfaces\HookInterface;
+use PBWebDev\CardanoPress\Blockfrost;
+use PBWebDev\CardanoPress\Profile;
 
 class Actions implements HookInterface
 {
+    protected Application $application;
+
+    public function __construct()
+    {
+        $this->application = Application::getInstance();
+    }
+
     public function setupHooks(): void
     {
+        add_action('cardanopress_associated_assets', [$this, 'checkAssets'], 10, 2);
+    }
+
+    protected function checkTokenQuantityAccess(string $id, int $quantity): void
+    {
+        $userProfile = new Profile(wp_get_current_user());
+        $conditions = array_filter($this->application->option('token_quantity_access'), function ($access) {
+            return (
+                ! empty($access['role']) &&
+                ! empty($access['id']) &&
+                ! empty($access['count'])
+            );
+        });
+        $accessIds = array_column($conditions, 'id');
+
+        foreach (array_keys($accessIds, $id, true) as $index) {
+            if (
+                $userProfile->hasRole($conditions[$index]['role']) ||
+                $quantity < $conditions[$index]['count']
+            ) {
+                continue;
+            }
+
+            $userProfile->addRole($conditions[$index]['role']);
+        }
+    }
+
+    protected function checkTokenAttributeAccess(array $data): void
+    {
+        $userProfile = new Profile(wp_get_current_user());
+        $conditions = array_filter($this->application->option('token_attribute_access'), function ($access) {
+            return (
+                ! empty($access['role']) &&
+                ! empty($access['location']) &&
+                ! empty($access['key']) &&
+                ! empty($access['value'])
+            );
+        });
+
+        foreach ($conditions as $condition) {
+            if ($userProfile->hasRole($condition['role']) || ! $this->tokenConditionMet($data, $condition)) {
+                continue;
+            }
+
+            $userProfile->addRole($condition['role']);
+        }
+    }
+
+    protected function tokenConditionMet(array $asset, array $condition): bool
+    {
+        $data = $asset[$condition['location']];
+
+        if (empty($data[$condition['key']])) {
+            return false;
+        }
+
+        return $data[$condition['key']] === $condition['value'];
+    }
+
+    public function checkAssets(string $stakeAddress, array $response): void
+    {
+        $queryNetwork = WalletHelper::getNetworkFromStake($stakeAddress);
+        $blockfrost = new Blockfrost($queryNetwork);
+
+        foreach ($response as $asset) {
+            $this->checkTokenQuantityAccess($asset['unit'], $asset['quantity']);
+
+            $data = $blockfrost->specificAsset($asset['unit']);
+
+            if (! empty($data)) {
+                $this->checkTokenAttributeAccess($data);
+            }
+        }
     }
 }
--- src/Admin.php
+++ src/Admin.php
@@ -19,5 +19,114 @@ class Admin extends AbstractAdmin
 
     public function setupHooks(): void
     {
+        add_action('plugins_loaded', function () {
+            $this->settingsPage('CardanoPress - Roles Manager', [
+                'parent' => Application::getInstance()->isReady() ? 'cardanopress' : '',
+                'menu_title' => 'Roles Manager',
+            ]);
+        });
+
+        add_action('init', function () {
+            $this->perAttributeFields();
+            $this->perQuantityFields();
+        });
+    }
+
+    protected function perAttributeFields(): void
+    {
+        $this->optionFields(__('Per Attribute', 'cardanopress-roles-manager'), [
+            'data_prefix' => 'token_attribute_',
+            'description' => __('Assigning a user role based on token attribute.', 'cardanopress-roles-manager'),
+            'fields' => [
+                'access' => [
+                    'type' => 'group',
+                    'default' => [
+                        [
+                            'name' => '',
+                            'role' => '',
+                            'location' => '',
+                            'key' => '',
+                            'value' => '',
+                        ],
+                    ],
+                    'repeatable' => true,
+                    'fields' => [
+                        'name' => [
+                            'type' => 'text',
+                            'description' => __('Name of the token.', 'cardanopress-roles-manager'),
+                            'title' => __('Asset Name', 'cardanopress-roles-manager'),
+                            'default' => 'tester'
+                        ],
+                        'role' => [
+                            'type' => 'select',
+                            'description' => __('Role to assign to user.', 'cardanopress-roles-manager'),
+                            'title' => __('Additional Role', 'cardanopress-roles-manager'),
+                            'options' => wp_roles()->role_names,
+                        ],
+                        'location' => [
+                            'type' => 'radio',
+                            'title' => __('Location', 'cardanopress-roles-manager'),
+                            'options' => [
+                                'onchain_metadata' => 'onchain_metadata',
+                                'metadata' => 'metadata',
+                            ],
+                        ],
+                        'key' => [
+                            'type' => 'text',
+                            'title' => __('Key', 'cardanopress-roles-manager'),
+                        ],
+                        'value' => [
+                            'type' => 'text',
+                            'title' => __('Value', 'cardanopress-roles-manager'),
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    protected function perQuantityFields(): void
+    {
+        $this->optionFields(__('Per Quantity', 'cardanopress-roles-manager'), [
+            'data_prefix' => 'token_quantity_',
+            'description' => __('Assigning a user role based on token quantity.', 'cardanopress-roles-manager'),
+            'fields' => [
+                'access' => [
+                    'type' => 'group',
+                    'default' => [
+                        [
+                            'name' => '',
+                            'role' => '',
+                            'id' => '',
+                            'count' => 1,
+                        ],
+                    ],
+                    'repeatable' => true,
+                    'fields' => [
+                        'name' => [
+                            'type' => 'text',
+                            'description' => __('Name of the token.', 'cardanopress-roles-manager'),
+                            'title' => __('Asset Name', 'cardanopress-roles-manager'),
+                            'default' => 'tester'
+                        ],
+                        'role' => [
+                            'type' => 'select',
+                            'description' => __('Role to assign to user.', 'cardanopress-roles-manager'),
+                            'title' => __('Additional Role', 'cardanopress-roles-manager'),
+                            'options' => wp_roles()->role_names,
+                        ],
+                        'id' => [
+                            'type' => 'text',
+                            'title' => __('Asset ID', 'cardanopress-roles-manager'),
+                        ],
+                        'count' => [
+                            'type' => 'number',
+                            'title' => __('Count', 'cardanopress-roles-manager'),
+                            'default' => 1,
+                        ],
+                    ],
+                ],
+            ],
+        ]);
     }
 }
